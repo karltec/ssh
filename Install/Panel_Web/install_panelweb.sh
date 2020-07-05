c90cd7ee0fbc398f6f07e795d68a6b6a9774a35c
CarlosMinho <56270160+CarlosMinho@users.noreply.github.com>
2020-07-05 00:57:06 -0300

    Update install_panelweb.sh

--- Install/Panel_Web/install_panelweb.sh
+++ Install/Panel_Web/install_panelweb.sh
@@ -3,4 +3,4 @@
 rm -rf $HOME/install_panelweb.sh
 rm -rf $HOME/Panelweb.sh
 
-wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/Panelweb.sh; chmod +x Panelweb.sh; ./Panelweb.sh
+wget https://raw.githubusercontent.com/karltec/ssh/master/Install/Panel_Web/Panelweb.sh?token=ANNJ2UD3AKKFCSJC4YBIZJC7AFH2U; chmod +x Panelweb.sh; ./Panelweb.sh
